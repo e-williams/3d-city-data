42b2616c7e644cb444bdfb01f0e2fee572bbdf3d
Evan Williams <Evan@users-MacBook-Air.local>
2024-03-19 13:31:43 -0700

    folder restructure

---
+++ public/build/routes/_index-3SWKGZAX.js
@@ -0,0 +1,64 @@
+import {
+  createHotContext
+} from "/build/_shared/chunk-QVG6GSVZ.js";
+import "/build/_shared/chunk-UWV35TSL.js";
+import {
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-XU7DNSPJ.js";
+import "/build/_shared/chunk-BOXFZXVX.js";
+import {
+  __toESM
+} from "/build/_shared/chunk-PNG5AS42.js";
+
+// app/routes/_index.tsx
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+if (!window.$RefreshReg$ || !window.$RefreshSig$ || !window.$RefreshRuntime$) {
+  console.warn("remix:hmr: React Fast Refresh only works when the Remix compiler is running in development mode.");
+} else {
+  prevRefreshReg = window.$RefreshReg$;
+  prevRefreshSig = window.$RefreshSig$;
+  window.$RefreshReg$ = (type, id) => {
+    window.$RefreshRuntime$.register(type, '"app/routes/_index.tsx"' + id);
+  };
+  window.$RefreshSig$ = window.$RefreshRuntime$.createSignatureFunctionForTransform;
+}
+var prevRefreshReg;
+var prevRefreshSig;
+if (import.meta) {
+  import.meta.hot = createHotContext(
+    //@ts-expect-error
+    "app/routes/_index.tsx"
+  );
+  import.meta.hot.lastModified = "1710654187949.0657";
+}
+function Index() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("p", { id: "index-page", children: [
+    "This is a demo for Remix.",
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("br", {}, void 0, false, {
+      fileName: "app/routes/_index.tsx",
+      lineNumber: 24,
+      columnNumber: 7
+    }, this),
+    "Check out",
+    " ",
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { href: "https://remix.run", children: "the docs at remix.run" }, void 0, false, {
+      fileName: "app/routes/_index.tsx",
+      lineNumber: 26,
+      columnNumber: 7
+    }, this),
+    "."
+  ] }, void 0, true, {
+    fileName: "app/routes/_index.tsx",
+    lineNumber: 22,
+    columnNumber: 10
+  }, this);
+}
+_c = Index;
+var _c;
+$RefreshReg$(_c, "Index");
+window.$RefreshReg$ = prevRefreshReg;
+window.$RefreshSig$ = prevRefreshSig;
+export {
+  Index as default
+};
+//# sourceMappingURL=/build/routes/_index-3SWKGZAX.js.map
