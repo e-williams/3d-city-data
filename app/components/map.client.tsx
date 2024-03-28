8e0ff1a385416110e412f9582cdccdfac6c297ec
Evan Williams <Evan@users-MacBook-Air.local>
2024-03-27 22:51:25 -0700

    added typed extension to map.client.tsx imports

--- app/components/map.client.tsx
+++ app/components/map.client.tsx
@@ -1,6 +1,6 @@
 import GLMap from 'react-map-gl';
-import { DeckGL } from '@deck.gl/react';
-import { LineLayer } from '@deck.gl/layers';
+import { DeckGL } from '@deck.gl/react/typed';
+import { LineLayer } from '@deck.gl/layers/typed';
 
 // Viewport settings
 const INITIAL_VIEW_STATE = {
